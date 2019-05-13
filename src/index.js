e928679d7e390709aa69de78798008baa7b06cbc
Alexander Oldenburg <alexanderoldenburg@MacBook-Pro-van-Alexander.local>
2019-05-13 16:59:31 +0200

    initial setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div className="container">LET'S GET THIS THING STARTED</div>,
+    document.getElementById('bankapp')
+);
+
+module.hot.accept();
